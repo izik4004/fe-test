2bcbe10c24492cd0b0f897e9ddc72594088f7ea1
isaac <izik40004@gmail.com>
2024-06-24 15:18:39 +0100

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,34 @@
+
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+
+  return (
+    <main className="">
+      <Sidebar  />
+      <div className="">
+        <div className="">
+          <Image
+            src="/icons/logo.svg"
+            width={30}
+            height={30}
+            alt="menu icon"
+          />
+          <div>
+            <MobileNav
+               />
+          </div>
+        </div>
+        {children}
+      </div>
+
+    </main>
+  )
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+
+const Home = () => {
+
+  return (
+    <section className=''>
+        <div className=''>
+            <header className=''>
+               
+            </header>
+        </div>
+
+
+    </section>
+  )
+}
+
+export default Home
